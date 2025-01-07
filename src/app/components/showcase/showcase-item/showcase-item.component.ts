f2f4aeb877ae4bd3151dc19940c2e1d7aadd1bb3
emmanuel macharia <samuelmarsha@outlook.com>
2025-01-07 12:33:28 +0300

    adds responsiveness

--- src/app/components/showcase/showcase-item/showcase-item.component.ts
+++ src/app/components/showcase/showcase-item/showcase-item.component.ts
@@ -1,13 +1,18 @@
 import { Component, input } from '@angular/core';
 import { IProject } from '../../../core/models/project';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @Component({
   selector: 'app-showcase-item',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './showcase-item.component.html',
   styleUrl: './showcase-item.component.scss',
 })
 export class ShowcaseItemComponent {
+  github=faGithub;
+  link=faArrowUpRightFromSquare;
   project = input.required<IProject>();
 }
