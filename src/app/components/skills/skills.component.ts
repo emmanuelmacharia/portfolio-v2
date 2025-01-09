87e6b7b8a19a1984572db96b1aab31fa4bbd9c23
emmanuel macharia <samuelmarsha@outlook.com>
2025-01-09 14:29:24 +0300

    more design fixes

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component,  } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBiohazard } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-skills',
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss',
 })
@@ -11,7 +13,7 @@ export class SkillsComponent{
   sections: Map<string, string[]> = new Map([
       ["languages", ["Python", "Typescript", "Javascript", "C#", "HTML", "CSS/SCSS", "SQL"]],
       ["frameworks & libraries", ["Angular", "React", "Next", "Node", "Flask", ".NET Framework", "Express", "Angular Material", "Bootstrap",  "Tailwind css", "MUI"]],
-      ["AI & data", ["TensorFlow", "PyTorch", "Scikit-learn", "Matplotlib", "Pandas", "Numpy"]],
+      ["AI & data", ["TensorFlow", "Keras", "PyTorch", "Scikit-learn", "Matplotlib", "Pandas", "Numpy"]],
       ["tools", ["Git & Github", "Bash", "Powershell", "Postman", "Markdown", "Anaconda"]],
       ["devops", ["Docker", "Kubernetes","Azure", "Azure Pipelines", "Heroku", "Gh-Actions", "Netlify" ]],
       ["databases", ["SQL Server", "MySql", "MongoDB", "PostgreSQL", "Redis"]]
@@ -19,4 +21,6 @@ export class SkillsComponent{
   )
 
   categories = ['languages', 'frameworks & libraries', 'AI & data', 'tools', 'devops', 'databases'];
+
+  bulletIcon = faBiohazard;
 }
